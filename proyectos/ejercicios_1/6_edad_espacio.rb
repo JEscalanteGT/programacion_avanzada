51d506926d5bf08b6637970dc5564910cd53b7fd
Jorge <url@jescalante.me>
2018-09-23 16:05:56 -0600

    Agrega el ejercicio edad en el espacio

---
+++ proyectos/ejercicios_1/6_edad_espacio.rb
@@ -0,0 +1,12 @@
+puts 'Ingrese su edad:'
+e = gets.chomp.to_i
+
+puts 'Tu edad en el espacio:'
+puts "Mercurio - #{e / 0.2408467} años"
+puts "Venus - #{e / 0.61519726} años"
+puts "Tierra - #{e} años"
+puts "Marte - #{e / 1.8808158} años"
+puts "Júpiter - #{e / 11.862615} años"
+puts "Saturno - #{e / 29.447498} años"
+puts "Urano - #{e / 84.016846} años"
+puts "Neptuno - #{e / 164.79132} años"
